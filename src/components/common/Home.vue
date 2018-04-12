c86d82dd3d922c785c4322f3856045ad3881f31f
jkwebDevelopment <35681341+jkwebDevelopment@users.noreply.github.com>
2018-04-12 14:39:40 +0800

    Add files via upload

---
+++ src/components/common/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <v-head></v-head>
+    <v-sidebar></v-sidebar>
+    <div class="content">
+      <transition name="move" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script type="es6">
+    import vHead from './Header.vue';
+    import vSidebar from './Sidebar.vue';
+    export default {
+      components:{
+        vHead,
+        vSidebar
+      }
+    }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/common/Home';
+import DashBoard from '@/components/page/DashBoard';
+
+Vue.use(Router);
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      children:[
+        {
+          path:'',
+          component:DashBoard
+        },{
+          path:'/DashBoard',
+          component:DashBoard
+        }
+      ]
+    }
+  ]
+})
